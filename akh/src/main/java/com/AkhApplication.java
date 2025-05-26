6f3acf11a14bfa31117f7c40aa684dff60a11d62
GTK18062005 <tharunkumargangavarapu@gmail.com>
2025-05-26 15:56:11 +0530

    Add files via upload

---
+++ akh/src/main/java/com/AkhApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkhApplication.class, args);
+	}
+
+}
